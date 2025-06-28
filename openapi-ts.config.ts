dd6dce5dbcd2ae892ad764dcd9ab2c3f8d03040b
nhat.th <hoangnhat257@gmail.com>
2025-06-28 18:20:25 +0700

    ...

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
-  base: "https://localhost:7198",
-  client: "axios",
-  input: "https://localhost:7198/swagger/v1.0/swagger.json",
-  output: {
-    lint: "eslint",
-    format: "prettier",
-    path: "./src/api/user",
-  },
-  types: {
-    enums: "typescript",
-  },
+    base: "https://localhost:7198",
+    // client: "axios", // Lỗi: 'client' does not exist in type 'Configs'
+    input: "https://localhost:7198/swagger/v1.0/swagger.json",
+    output: {
+        lint: "eslint",
+        format: "prettier",
+        path: "./src/api/user",
+    },
+    // types: {
+    //     enums: "typescript",
+    // },
 });
